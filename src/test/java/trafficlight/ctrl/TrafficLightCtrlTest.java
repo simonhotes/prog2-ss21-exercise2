ec69066887ad110088ab339cdef858b401d789f2
simonhotes <92045737+simonhotes@users.noreply.github.com>
2022-04-03 22:23:17 +0200

    First Commit

---
+++ src/test/java/trafficlight/ctrl/TrafficLightCtrlTest.java
@@ -0,0 +1,32 @@
+package trafficlight.ctrl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrafficLightCtrlTest {
+
+    @Test
+    void getGreenState() {
+    }
+
+    @Test
+    void getRedState() {
+    }
+
+    @Test
+    void getYellowState() {
+    }
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void nextState() {
+    }
+
+    @Test
+    void stop() {
+    }
+}
---
+++ src/test/java/trafficlight/states/StateTest.java
@@ -0,0 +1,23 @@
+package trafficlight.states;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StateTest {
+
+    @Test
+    @DisplayName("Method getStateNext incorrect.")
+    void getNextState() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void getSting() {
+    }
+}
